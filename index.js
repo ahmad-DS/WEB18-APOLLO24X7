3ecec41046e558aac6438b885aa67cd401c6ea07
Anshul-Singla <asingla2807@gmail.com>
2022-05-07 15:55:42 +0530

    some changes

--- index.js
+++ index.js
@@ -8,3 +8,31 @@ function showRemaining(){
         sDivs[i].className = ".showHideDiv"
     } 
 }
+var imgIndex = 1;
+showImage(imgIndex);
+function imgSlider(n){
+    imgIndex += n;
+    // console.log(imgIndex,"b",n)
+    showImage(imgIndex)
+    // console.log(imgIndex,"a",n)
+
+}
+function currentSlide(n){
+    // imgIndex = n;
+    showImage(n)
+}
+function showImage(n){
+    var images = document.querySelectorAll(".carousel_image");
+    // console.log(images)
+    if(n<1){
+        imgIndex = images.length;
+        n = images.length;
+    }else if(n>images.length){
+        imgIndex=1;
+        n=1;
+    }
+    for(var i=0 ; i<images.length ; i++){
+        images[i].style.display ="none";
+    }
+    images[n-1].style.display ="block";
+}
