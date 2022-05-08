4d3cfa9b4c5e7e76bca6300b75422e6b5e1860ac
Shakil Ahmad <ahmad.shakil444@gmail.com>
2022-05-08 10:57:44 +0530

    cart page updated again

--- cart/cart.js
+++ cart/cart.js
@@ -116,6 +116,8 @@ displayData(cartItems);
 function displayData(data) {
     document.querySelector("#added_items").innerHTML = "";
     document.querySelector("#total_items").innerText=data.length;
+    document.querySelector("#cart_logo_top").innerText=data.length;
+
     data.forEach(function (element,index) {
         var card = document.createElement("div");
         card.setAttribute("class", "card");
@@ -177,6 +179,9 @@ function displayData(data) {
             
             cartItems.splice(index,1);
             displayData(cartItems);
+            totalSavings();
+            totalAmount();
+            payableAmount()
             // localStorage.set
         })
         del.setAttribute("class","delete")
